88edd6c552a9371222953b587b97476484bad467
mmparksb <mmparksb@gmail.com>
2024-10-24 18:24:00 +0900

    SuperDao 수정

--- ShoppingMallProject/src/main/java/com/shopping/dao/SuperDao.java
+++ ShoppingMallProject/src/main/java/com/shopping/dao/SuperDao.java
@@ -1,5 +1,32 @@
 package com.shopping.dao;
 
-public class SuperDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class SuperDao {
+	protected Connection conn = null ; 
+	
+	public Connection getConnection() {
+		try {
+			String url = "jdbc:oracle:thin:@localhost:1521:xe" ;
+			String id = "projectV2" ;
+			String password = "oracle" ;
+			
+			this.conn = DriverManager.getConnection(url, id, password) ;
+			if(conn == null) {System.out.println("접속 실패");} 			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return conn ;
+	}	
+	
+	public SuperDao() {
+		String driver = "oracle.jdbc.driver.OracleDriver" ;
+		try {
+			Class.forName(driver) ;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ ShoppingMallProject/src/main/java/com/shopping/model/Member.java
@@ -0,0 +1,17 @@
+package com.shopping.model;
+
+public class Member {
+	private long member_id;
+	private String signup_id;
+	private String member_name;
+	private String member_nickname;
+	private String password;
+	private String member_email;
+	private String phone_number;
+	private String created_at;
+	private String updated_at;
+	private String last_login;
+	private int status; // 1이 true / 0이 false
+	private String birthDay;
+	private long point;
+}
