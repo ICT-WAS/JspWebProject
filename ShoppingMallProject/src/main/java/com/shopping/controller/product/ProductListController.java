3fcafb7214fe40ab7295a460c7790fcd1eb1f34e
hw <hw710204@gmail.com>
2024-10-24 21:10:29 +0900

    dao 기능 수정 / 상품 모델 선언 / 상품 상세 페이지 연결(컨트롤러)

--- ShoppingMallProject/src/main/java/com/shopping/controller/product/ProductListController.java
+++ ShoppingMallProject/src/main/java/com/shopping/controller/product/ProductListController.java
@@ -1,5 +1,9 @@
 package com.shopping.controller.product;
 
+//import javax.servlet.annotation.WebServlet;
+
+//@WebServlet("/product/list")
 public class ProductListController {
+	
 
 }
--- ShoppingMallProject/src/main/java/com/shopping/dao/ProductDao.java
+++ ShoppingMallProject/src/main/java/com/shopping/dao/ProductDao.java
@@ -1,5 +1,68 @@
 package com.shopping.dao;
 
-public class ProductDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shopping.model.Product;
+
+
+public class ProductDao extends SuperDao{
+	
+	//전체 상품 목록 가져오기
+	public List<Product> getProductList(){
+		List<Product> productList = new ArrayList<>();
+		
+		String sql = " SELECT * FROM PRODUCTS";
+		
+		try(Connection conn = super.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery()) {
+			
+			while (rs.next()) {
+				productList.add(getBeanData(rs));
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return productList;
+	}
+
+	
+	
+	//productId에 매칭되는 상품 한 개 가져오기
+	public Product getProductById(long productId) {
+		Product product = null;
+		String sql = " SELECT * FROM PRODUCT WHERE PRODUCT_ID = ?";
+		
+		try(Connection conn = super.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql)){
+			
+			pstmt.setLong(0, productId);
+			ResultSet rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				product = getBeanData(rs);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return product;  //조회된 상품 반환, 찾지 못하면 null
+	}
+	
+	
+	
+	//예외처리 받아서 처리하는 곳
+	private Product getBeanData(ResultSet rs) throws SQLException{
+		Product product = new Product();
+		return product;
+	}
 
 }
