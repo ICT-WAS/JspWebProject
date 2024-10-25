6cca17f9dc64fd34cddacb635734de17ac09c484
mmparksb <mmparksb@gmail.com>
2024-10-25 14:53:07 +0900

    Update ProductDao.java

--- ShoppingMallProject/src/main/java/com/shopping/dao/ProductDao.java
+++ ShoppingMallProject/src/main/java/com/shopping/dao/ProductDao.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import com.shopping.model.Product;
 
-
 public class ProductDao extends SuperDao{
 	
 	//전체 상품 목록 가져오기
@@ -32,8 +31,6 @@ public class ProductDao extends SuperDao{
 		
 		return productList;
 	}
-
-	
 	
 	//productId에 매칭되는 상품 한 개 가져오기
 	public Product getProductById(long productId) {
@@ -56,13 +53,10 @@ public class ProductDao extends SuperDao{
 		
 		return product;  //조회된 상품 반환, 찾지 못하면 null
 	}
-	
-	
-	
+
 	//예외처리 받아서 처리하는 곳
 	private Product getBeanData(ResultSet rs) throws SQLException{
 		Product product = new Product();
 		return product;
 	}
-
 }
